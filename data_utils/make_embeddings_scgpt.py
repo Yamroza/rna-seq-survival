3ac5bf9463b44180f659f27ad35df076c590b53f
Yamroza <oyamroz@gmail.com>
2026-03-02 19:20:52 +0100

    Mixture of experts test

--- data_utils/make_embeddings_scgpt.py
+++ data_utils/make_embeddings_scgpt.py
@@ -13,11 +13,11 @@ import pandas as pd
 
 warnings.simplefilter("ignore", ResourceWarning)
 
-filenames = ["adata_TCGA-LUAD.star_tpm", "adata_TCGA-BRCA.star_tpm"] # source data file, in "data" folder
+filenames = ["adata_TCGA-BRCA.star_tpm"] # source data file, in "data" folder "adata_TCGA-LUAD.star_tpm", 
 models = ["whole_human"] #, "pancancer"]
-# highly_variables = [(False, 0)]#, (True, 3000), (True, 6000)]
-highly_variables = [(True, 3000), (True, 6000)]
-
+highly_variables = [(False, 0)]#, (True, 3000), (True, 6000)]
+# highly_variables = [(True, 3000), (True, 6000)]
+iter = range(4,20)
 # max_length = 10000
 
 for filename in filenames:
@@ -25,39 +25,36 @@ for filename in filenames:
     adata = sc.read_h5ad(smaple_data_path)
     gene_col = "gene_name"
     batch_key = "sample"
-    print(adata.X[:5, :5])  # jakie wartości?
-    print(adata.X.max(), adata.X.min())
-    print(adata.uns)  # czy jest info o transformacji?
-    raise(KeyError)
-    for model in models:
-        for highly_variable in highly_variables:
-            is_hvg, hvg_no = highly_variable
-            if is_hvg:
-                sc.pp.highly_variable_genes(adata, n_top_genes=hvg_no, flavor='seurat_v3')
-                data_for_emb = adata[:, adata.var['highly_variable']]
-            else:
-                data_for_emb = adata
-            
-            model_dir = Path(f"/scratch/2370352/my-research/papers/scgpt/save/{model}")
-            embed_adata = scg.tasks.embed_data(
-                data_for_emb,
-                model_dir,
-                gene_col=gene_col,
-                batch_size=32,
-                max_length= hvg_no,
-            )
+    for i in iter:
+        for model in models:
+            for highly_variable in highly_variables:
+                is_hvg, hvg_no = highly_variable
+                if is_hvg:
+                    sc.pp.highly_variable_genes(adata, n_top_genes=hvg_no, flavor='seurat_v3')
+                    data_for_emb = adata[:, adata.var['highly_variable']]
+                else:
+                    data_for_emb = adata
+                
+                model_dir = Path(f"/scratch/2370352/my-research/papers/scgpt/save/{model}")
+                embed_adata = scg.tasks.embed_data(
+                    data_for_emb,
+                    model_dir,
+                    gene_col=gene_col,
+                    batch_size=32,
+                    # max_length= hvg_no,
+                )
 
-            # zapis embeddingów do pliku
+                # zapis embeddingów do pliku
 
-            X_emb = embed_adata.obsm["X_scGPT"]
-            emb_cols = [f"emb{i+1}" for i in range(X_emb.shape[1])]
-            samples = embed_adata.obs.index
-            df_emb = pd.DataFrame(
-                X_emb,
-                index=samples,
-                columns=emb_cols
-            )
-            df_emb = df_emb.reset_index().rename(columns={"index": "Unnamed: 0"})
+                X_emb = embed_adata.obsm["X_scGPT"]
+                emb_cols = [f"emb{i+1}" for i in range(X_emb.shape[1])]
+                samples = embed_adata.obs.index
+                df_emb = pd.DataFrame(
+                    X_emb,
+                    index=samples,
+                    columns=emb_cols
+                )
+                df_emb = df_emb.reset_index().rename(columns={"index": "Unnamed: 0"})
 
-            # Path(f"../data/scgpt_embeddings/{max_length}").mkdir(parents=True, exist_ok=True)
-            df_emb.to_csv(f"../data/scgpt_embeddings/new_lengths/scgpt_{filename}_{model}_hvg_{hvg_no}_ml_{hvg_no}.csv", index=False)
+                # Path(f"../data/scgpt_embeddings/{max_length}").mkdir(parents=True, exist_ok=True)
+                df_emb.to_csv(f"../data/scgpt_embeddings/new_lengths/scgpt_{filename}_{model}_hvg_{hvg_no}_ml_1200_{i}.csv", index=False)
